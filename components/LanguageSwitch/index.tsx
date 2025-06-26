7e7c4bd6480510c7e6cf7995d9e5d6b7ab8db594
Vikram Kumar <vikcanada90@gmail.com>
2025-06-26 08:07:00 -0600

    fixed issue with language dropdown sigze

--- components/LanguageSwitch/index.tsx
+++ components/LanguageSwitch/index.tsx
@@ -37,10 +37,10 @@ export default function LanguageSwitch() {
   const selected = languages.find((l) => l.code === lang)!;
 
   return (
-    <Dropdown type="menu">
+    <Dropdown type="menu" className="min-w-fit">
       <DropdownTrigger>
         <div className="hover:cursor-pointer">
-          <span className="text-md mr-1">{selected.flag}</span>
+          <span className="text-md">{selected.flag}</span>
           {selected.label}
         </div>
       </DropdownTrigger>
@@ -49,13 +49,13 @@ export default function LanguageSwitch() {
         selectionMode="single"
         selectedKeys={selectedKeys}
         onSelectionChange={(keys) => handleSelectionChange(Array.from(keys))}
-        className="w-30"
+        className="min-w-fit"
       >
         {languages
           .filter((l) => l.code !== lang)
           .map((lang) => (
             <DropdownItem key={lang.code}>
-              <span className="text-md mr-1">{lang.flag}</span>
+              <span className="text-md">{lang.flag}</span>
               {lang.label}
             </DropdownItem>
           ))}
